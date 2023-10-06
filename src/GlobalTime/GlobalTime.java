44b6fd59a9c15344c9e7ebb94880c89189e9015f
YoungHeeSo <apdlfdldjqtm@gmail.com>
2023-10-06 21:32:01 +0900

    # 시간 수정

--- src/GlobalTime/GlobalTime.java
+++ src/GlobalTime/GlobalTime.java
@@ -1,41 +1,19 @@
 package GlobalTime;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class GlobalTime {
 
+    protected void globalTime(String name, String country){
 
-    protected String globalTime(String country){
         ZoneId zonCountry = ZoneId.of(country);
         ZonedDateTime kt = ZonedDateTime.now(zonCountry);
-
         DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
 
-        return kt.format(timeFormat);
-    }
+        System.out.print(name + " | " + kt.format(timeFormat) + "\t"+"\t");
 
-    protected String KoreanTime(){
-
-        ZoneId zonk = ZoneId.of("Asia/Seoul");
-        ZonedDateTime kt = ZonedDateTime.now(zonk);
-
-        DateTimeFormatter ktfm = DateTimeFormatter.ofPattern("z HH시 mm분 ss초");
-        String timeformatkor = kt.format(ktfm);
-
-        return timeformatkor;
-    }
-    protected String ChinaTime(){
-        ZoneId zonk = ZoneId.of("Asia/Shanghai");
-        ZonedDateTime kt = ZonedDateTime.now(zonk);
-
-        DateTimeFormatter ktfm = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
-        String timeformatchi = kt.format(ktfm);
-
-        return timeformatchi;
     }
 
 }
--- src/GlobalTime/Main.java
+++ src/GlobalTime/Main.java
@@ -5,25 +5,24 @@ public class Main {
 
         GlobalTime gt =  new GlobalTime();
 
-//            gt.KoreanTime();
         while (true){
-//            System.out.print("한국 시간: "+gt.KoreanTime()+ " || "+ "중국 시간: "+gt.ChinaTime());
-            System.out.print("한국 시간: " + gt.globalTime("Asia/Seoul") + " || ");
-            System.out.print("중국 시간: " + gt.globalTime("Asia/Shanghai") + " || ");
-            System.out.print("일본 시간: " + gt.globalTime("Asia/Tokyo") + " || ");
-            System.out.print("벨기에 시간: " + gt.globalTime("Europe/Brussels") + " || ");
-            System.out.print("독일 시간: " + gt.globalTime("Europe/Berlin"));
+            gt.globalTime("한국","Asia/Seoul");
+            gt.globalTime("중국","Asia/Shanghai");
+            gt.globalTime("일본","Asia/Tokyo");
+            gt.globalTime("미국","America/Chicago");
+            gt.globalTime("독일","Europe/Berlin");
 
-            //            System.out.println();
-//            System.out.print("중국 시간: "+gt.ChinaTime());
             System.out.print("\r");
-//            System.out.println();
 
             try{
                 Thread.sleep(1000);
+
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                e.fillInStackTrace();
             }
+
         }
+
     }
+
 }
